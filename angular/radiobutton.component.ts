833706ae964543916f2b0b6b77605b620dd02193
Mark Hatchell <mark.hatchell@prospect33.com>
2017-01-31 19:58:04 -0500

    created radiobutton component for angular

---
+++ angular/radiobutton.component.ts
@@ -0,0 +1,24 @@
+import {
+    Component, ChangeDetectionStrategy, ElementRef
+} from "@angular/core";
+
+import { RadioButton } from '../index';
+
+@Component({
+    selector: "RadioButton",
+    template: "",
+    changeDetection: ChangeDetectionStrategy.OnPush
+})
+
+export class RadioButtonComponent {
+
+    private _native: RadioButton;
+
+    constructor(
+        private _element: ElementRef
+    ) {
+        this._native = this._element.nativeElement;
+    }
+
+}
+
