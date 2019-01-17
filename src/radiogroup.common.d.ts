e346a5189c7e2ce057a9a9a214625b65c763b6a8
Rajiv Narayana Singaseni <rajivnarayana@gmail.com>
2019-01-17 10:09:07 +0530

    Changes for using ngc to pre compile the build for Angular.

--- src/radiogroup.common.d.ts
+++ src/radiogroup.common.d.ts
@@ -1,16 +1,12 @@
-import { Property, View } from 'tns-core-modules/ui/core/view';
 import { EventData } from "tns-core-modules/data/observable";
-
 export interface SelectedEventData extends EventData {
-  object: any;
-  checkId: number;
-  value: string;
+    object: any;
+    checkId: number;
+    value: string;
 }
-
 export interface RadioGroupInterface {
     checkedButton: number;
 }
-
 export interface RadioButtonInterface {
     text?: string;
     checked: boolean;
